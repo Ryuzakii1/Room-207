7ee9d9a34ad087d1998695e142d96cb8858fe9fd
Ryuzakii1 <blizzard579@gmail.com>
2020-04-23 23:30:08 -0700

    Initial Commit
    Currently figuring out a unity tutorial

---
+++ Assets/Scripts/DamageCollidable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageCollidable : MonoBehaviour
+{
+    void OnTriggerStay2D(Collider2D other)
+    {
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if (controller != null)
+        { 
+            {
+                controller.ChangeHealth(-1);
+            }
+        }
+    }
+
+
+
+
+
+}
